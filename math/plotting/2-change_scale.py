9f68f3de36834035e9adcaf15d87a3162d9e961e
Christian Lopez <christianlopezrivera0@gmail.com>
2025-01-15 18:00:19 -0800

    machine learning ploting 0-2

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -3,10 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def change_scale():
-    x = np.arange(0, 28651, 5730)  # Time points from 0 to 28650, step size of 5730
-    r = np.log(0.5)  # Natural log of 0.5 (decay factor for C-14)
+    x = np.arange(0, 28651, 5730)  # Time points from 0 to 28650 with a step size of 5730
     t = 5730  # Half-life of C-14 in years
-    y = np.exp((r / t) * x)  # Exponential decay formula
+    y = 0.5**(x / t)  # Exponential decay function (Fraction Remaining)
 
     plt.figure(figsize=(6.4, 4.8))
 
@@ -20,6 +19,9 @@ def change_scale():
 
     # Set the y-axis to a logarithmic scale
     plt.yscale("log")
+
+    # Set the x-axis range from 0 to 28650
+    plt.xlim(0, 28650)
 
     # Show the plot
     plt.show()
