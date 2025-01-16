70c447a484cd3f1b5328133a322224664114c5b0
Christian Lopez <christianlopezrivera0@gmail.com>
2025-01-15 17:41:01 -0800

    machine learning ploting 0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def line():
-    y = np.arange(0, 11) ** 3
-    x = np.arange(0, 11)  # x values from 0 to 10
+    y = np.arange(0, 11) ** 3  # y = x^3 for x in [0, 10]
+    x = np.arange(0, 11)       # x values from 0 to 10
 
     plt.figure(figsize=(6.4, 4.8))
 
@@ -13,6 +13,9 @@ def line():
 
     # Set x-axis range from 0 to 10
     plt.xlim(0, 10)
+
+    # Ensure the y-axis also adjusts according to the cubic values
+    plt.ylim(min(y), max(y))
 
     # Add title and labels
     plt.title('y = x^3')
