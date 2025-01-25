029acbaa18f09b9daac7cdbf930f088d463f682f
Christian Lopez <christianlopezrivera0@gmail.com>
2025-01-25 15:03:56 -0800

    machine learning optimization 2

---
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    Optimization project
+"""
+import numpy as np
+
+
+def shuffle_data(X, Y):
+    """ Shuffles data points in two matrices the same way
+        X is ndarray with shape (m nx)
+            m number of data points
+            nx number of features
+        Y is ndarray with shape (m nx)
+            m number of data points
+            nx number of features
+        Returns: shuffled X, Y matrices
+    """
+    permute = np.random.permutation(X.shape[0])
+    return X[permute], Y[permute]
